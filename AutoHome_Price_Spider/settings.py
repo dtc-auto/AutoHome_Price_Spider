5c7ce1bc9b59336cb53005cf2d2d1fe9cea39935
Ge Jianyu <1>
2018-02-01 14:49:32 +0800

    取消机器人规则

--- AutoHome_Price_Spider/settings.py
+++ AutoHome_Price_Spider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'AutoHome_Price_Spider.spiders'
 #USER_AGENT = 'AutoHome_Price_Spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
